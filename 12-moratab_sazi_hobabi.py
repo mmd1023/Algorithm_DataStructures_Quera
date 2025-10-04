ac334914469d877e050d1c26e45dd75127bc7df6
MMD <mmd.gh313@gmail.com>
2025-10-05 02:37:13 +0330

    add solution 12

---
+++ 12-moratab_sazi_hobabi.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+print(*bubble_sort(arr))
